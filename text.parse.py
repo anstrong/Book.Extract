a05cb4a5bc798fb0f0ee589ae92fee4aa994923c
Annabelle Strong <anstrong@users.noreply.github.com>
2017-12-04 01:09:29 -0500

    Update text.parse.py

--- text.parse.py
+++ text.parse.py
@@ -1,77 +1,68 @@
 from urllib.request import Request, urlopen
 from bs4 import BeautifulSoup, SoupStrainer
+from gooey import Gooey
+from argparse import ArgumentParser
+
+@Gooey(program_name="Book Extractor 1.0")
 
 # https://www.8novels.net/classics/u6082.html
 
-# Get url
-y = input("What's the url of the book you want to parse? ")
+def main():
+	parser = ArgumentParser()
 
-# Get number of pages
-x = int(input("How many pages in the book? "))
+	parser.add_argument('Book_Name', help="Name to save text as")
+	parser.add_argument('Number_of_Pages', help="Number of pages over which the text is distributed")
+	parser.add_argument('URL', help="URL of the text you want to parse")
 
-# Get name
-z = input("What's the name of the book? ")
-
-# Create .txt file
-path = '/Users/annabelle_strong/Documents/Bin/Extracted Texts/'
-filename = str(z)
-
-file = open(path + filename,"w") 
-
-# Split url
-f = y.rfind('_')
-g = y.rfind('/')
-
-if f == -1:
-	start1 = y.rfind('/') + 1
-	end1 = y.find('.html')
-	code = y[start1:end1]
-
-elif f !=-1 and (f < g):
-	start1 = y.rfind('/') + 1
-	end1 = y.find('.html')
-	code = y[start1:end1]
-
-else:
-	start1 = y.rfind('/') + 1
-	end1 = y.rfind('_') 
-	code = y[start1:end1]
-
-start = y.find('www.')
-end = y.find(str(code)) 
-path = y[start:end]
-
-# Loop through pages
-for a in range(1, x+1):
-	# Use split url to form complete url
-	if a == 1:
-		url = Request('http://' + str(path) + str(code) + '.html', headers={'User-Agent': 'Mozilla/5.0'})
-	else:
-		url = Request('http://' + str(path) + str(code) + '_' + str(a) +'.html', headers={'User-Agent': 'Mozilla/5.0'})
+	args = parser.parse_args()
 	
-	# Read html
-	html = urlopen(url).read()
+	# Create .txt file
+	file = open(str(args.Book_Name)+".txt","w") 
+	
+	# Get url
+	y = str(args.URL)
+	
+	# Get number of pages
+	x = int(args.Number_of_Pages)
 
-	# Filter paragraph text and parse page
-	p = SoupStrainer("p")
+	# Split url
+	start = y.find('http://')
+	end = y.find('index') + 5
+	path = y[start:end]
 
-	soup = BeautifulSoup(html, "html.parser", parse_only=p)
+	# Loop through pages
+	for a in range(1, x+1):
+		# Use split url to form complete url
+		if a == 1:
+			url = Request('http://' + str(path) + str(code) + '.html', headers={'User-Agent': 'Mozilla/5.0'})
+		else:
+			url = Request('http://' + str(path) + str(code) + '_' + str(a) +'.html', headers={'User-Agent': 'Mozilla/5.0'})
 
-	# Remove all script and style elements
-	for script in soup(["script", "style"]):
-	    script.extract()    # rip it out
+		# Read html
+		html = urlopen(url).read()
 
-	# Remove copyright line
-	for p in soup.find_all("p", {'class':'info'}): 
-	    p.decompose()
+		# Filter paragraph text and parse page
+		p = SoupStrainer("p")
 
-	# Extract text
-	text = soup.get_text()
+		soup = BeautifulSoup(html, "html.parser", parse_only=p)
 
-	# Write text to document
-	file.write(text)
+		# Remove all script and style elements
+		for script in soup(["script", "style"]):
+		    script.extract()    # rip it out
 
-print("Text extraction complete.")
+		# Remove copyright line
+		for p in soup.find_all("p", {'class':'info'}): 
+		    p.decompose()
+
+		# Extract text
+		text = soup.get_text()
+
+		# Write text to document
+		file.write(text)
+
+	print("Text extraction complete.")
+	
+main()
 
 
 
